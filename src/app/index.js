840c20406cec3823627eb77c53400ca860a42cdf
pashaGhub <pawelsucharew@gmail.com>
2019-10-03 01:44:54 +0300

    app responcivity using grid added && basic layout created

--- src/app/index.js
+++ src/app/index.js
@@ -1,7 +1,75 @@
-import React from 'react';
+import React from "react";
+import "./index.scss";
 
 function App() {
-    return <div>Hello here</div>
+  return (
+    <React.Fragment>
+      <div className="Site">
+        <header className="Header">there will be header</header>
+        <nav className="Side-nav">
+          <ul>
+            <li>first</li>
+            <li>second</li>
+            <li>third</li>
+            <li>fourth</li>
+          </ul>
+        </nav>
+        <main className="Main">
+          That'll create a grid that's four columns wide by three rows tall. The
+          entire top row will be comprised of the header area. The middle row
+          will be comprised of two main areas, one empty cell, and one sidebar
+          area. The last row is all footer.That'll create a grid that's four
+          columns wide by three rows tall. The entire top row will be comprised
+          of the header area. The middle row will be comprised of two main
+          areas, one empty cell, and one sidebar area. The last row is all
+          footer.That'll create a grid that's four columns wide by three rows
+          tall. The entire top row will be comprised of the header area. The
+          middle row will be comprised of two main areas, one empty cell, and
+          one sidebar area. The last row is all footer.That'll create a grid
+          that's four columns wide by three rows tall. The entire top row will
+          be comprised of the header area. The middle row will be comprised of
+          two main areas, one empty cell, and one sidebar area. The last row is
+          all footer.That'll create a grid that's four columns wide by three
+          rows tall. The entire top row will be comprised of the header area.
+          The middle row will be comprised of two main areas, one empty cell,
+          and one sidebar area. The last row is all footer.That'll create a grid
+          that's four columns wide by three rows tall. The entire top row will
+          be comprised of the header area. The middle row will be comprised of
+          two main areas, one empty cell, and one sidebar area. The last row is
+          all footer.That'll create a grid that's four columns wide by three
+          rows tall. The entire top row will be comprised of the header area.
+          The middle row will be comprised of two main areas, one empty cell,
+          and one sidebar area. The last row is all footer.That'll create a grid
+          that's four columns wide by three rows tall. The entire top row will
+          be comprised of the header area. The middle row will be comprised of
+          two main areas, one empty cell, and one sidebar area. The last row is
+          all footer.That'll create a grid that's four columns wide by three
+          rows tall. The entire top row will be comprised of the header area.
+          The middle row will be comprised of two main areas, one empty cell,
+          and one sidebar area. The last row is all footer.That'll create a grid
+          that's four columns wide by three rows tall. The entire top row will
+          be comprised of the header area. The middle row will be comprised of
+          two main areas, one empty cell, and one sidebar area. The last row is
+          all footer.That'll create a grid that's four columns wide by three
+          rows tall. The entire top row will be comprised of the header area.
+          The middle row will be comprised of two main areas, one empty cell,
+          and one sidebar area. The last row is all footer.That'll create a grid
+          that's four columns wide by three rows tall. The entire top row will
+          be comprised of the header area. The middle row will be comprised of
+          two main areas, one empty cell, and one sidebar area. The last row is
+          all footer.That'll create a grid that's four columns wide by three
+          rows tall. The entire top row will be comprised of the header area.
+          The middle row will be comprised of two main areas, one empty cell,
+          and one sidebar area. The last row is all footer.That'll create a grid
+          that's four columns wide by three rows tall. The entire top row will
+          be comprised of the header area. The middle row will be comprised of
+          two main areas, one empty cell, and one sidebar area. The last row is
+          all footer.
+        </main>
+        <footer className="Footer">there will be footer</footer>
+      </div>
+    </React.Fragment>
+  );
 }
 
 export default App;
